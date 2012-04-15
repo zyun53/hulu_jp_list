1eba7d0f79f62d866f09010e0a6e5ddcdc604c76
Ishida Junichi <junichi@i4d.jp>
2012-04-15 22:03:52 +0900

    Add sliqte3.

--- hulujp_list.pl
+++ hulujp_list.pl
@@ -6,6 +6,11 @@ use WWW::Mechanize;
 use Web::Scraper;
 use DBI;
 
+my $database = 'list.db';
+my $dbh = DBI->connect("dbi:SQLite:dbname=list.db");
+my $insert_sql = 'INSERT INTO content (url_name, title, season, episode, release, insert_date) values ( ?, ?, ?, ?, ?, ?);';
+my $sth = $dbh->prepare($insert_sql);
+
 my $mech = new WWW::Mechanize( autocheck => 1 );
 
 $mech->get('http://www2.hulu.jp/content');
@@ -25,17 +30,32 @@ for (my $i = 0; $i < $result_count; $i++) {
             process '.bold-link', title => 'TEXT';
             process '.bold-link', link => '@href';
             process 'span.season-episode-count' , append => 'TEXT';
+            process 'span.decades', release => 'TEXT';
         }
     };
 
     $res = $s->scrape($mech->content);
 
     foreach my $line (@{$res->{list}}) {
-        print encode('utf-8', $line->{title} . ' ' . $line->{link} . ' ');
+        $line->{link} =~ /\.jp\/(.+)$/;
+        my @insert_values = ( $1, $line->{title} ); #url_name, title
+
         if (defined($line->{append})) {
-            print encode('utf-8', $line->{append});
+            $line->{append} =~ /(\d+).*\|\D+(\d+)\D/;
+            push @insert_values, $1; #season
+            push @insert_values, $2; #episode
+        } else {
+            push @insert_values, '0'; #season
+            push @insert_values, '0'; #episode
         }
-        print "\n";
+
+
+        $line->{release} =~ /(\d+)/;
+        push @insert_values, $1;
+        push @insert_values, "20120403";
+
+        $sth->execute(@insert_values) or print "@insert_values";
+
     }
 
     $mech->follow_link(text => 'next');
---
+++ sql/sqlite.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS content (
+    url_name TEXT NOT NULL PRIMARY KEY,
+    title TEXT,
+    season INTEGER,
+    episode INTERGER,
+    release TEXT,
+    insert_date TEXT
+);
